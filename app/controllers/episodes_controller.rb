55ec6434cf22b18b99d51d5614922053744c06dc
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-01-18 11:14:12 -0700

    all deliverables except destroy and create

---
+++ app/controllers/episodes_controller.rb
@@ -0,0 +1,39 @@
+class EpisodesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def index 
+        episodes = Episode.all 
+        render json: episodes
+    end
+
+    def show
+        episode = find_episode_by_id
+        if episode
+            render json: episode
+        else 
+            render json: {error: "Episode not found"}, status: :not_found
+        end
+    end
+
+    def destroy
+        episode = find_episode
+        episode.destroy
+        head: :no_content
+    end
+    
+    private
+
+    def find_episode 
+        Episode.find(params[:id])
+    end
+
+    def find_episode_by_id
+        episode = Episode.find_by(id: params[:id])
+    end
+
+    def render_unprocessable_entity_response
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+
+end
